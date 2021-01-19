0654bdd4c53dfebc63e1d2ec971b0025f63e9b9e
Mike Rogers <me@mikerogers.io>
2021-01-19 20:47:14 +0000

    Adding User Model

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+require "faker"
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+    sequence :email do |n|
+      "test#{n}@example.com"
+    end
+    eye_colour { ["Greeb", "Blue", "Silver"].sample }
+  end
+end
