995c266ef81487f82871fdd9483929ccf1a040f8
Mike Rogers <me@mikerogers.io>
2021-01-20 16:23:23 +0000

    Adding in comments to make it clearer

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,6 @@
+# Preview all emails at http://localhost:3000/rails/mailers
 class UserMailerPreview < ActionMailer::Preview
+
   def user_welcome_notification
     UserMailer.user_welcome_notification(user)
   end
